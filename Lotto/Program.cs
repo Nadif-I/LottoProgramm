4d9da1f562f5e0285187d11adeda087b9ccd1661
Ilyass Nadif <ilyass.nadif@outlook.com>
2022-11-09 01:30:05 +0100

    1. commit to lotto rep

--- Lotto/Program.cs
+++ Lotto/Program.cs
@@ -3,7 +3,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, Lotto!");
+        Console.WriteLine("git add after init");
+
     }
 }
 
