1519f6ae5091749dabcecc6f1ca2f3740e78b7f2
Ilyass Nadif <ilyass.nadif@outlook.com>
2022-11-09 09:19:42 +0100

    do while

--- Lotto/Program.cs
+++ Lotto/Program.cs
@@ -1,9 +1,37 @@
 ﻿namespace Lotto;
+/*
+Schreiben Sie ein Lottoprogramm (6 aus 49)
+
+Dieses soll ein Array enthalten, das zufallsbasiert 6 Gewinnzahlen beinhaltet.
+(Nutzen Sie dazu ein random-Objekt).
+
+Nun sollte der Anwender 6 Zahlen aus 49 auswählen, die mit den Gewinnzahlen
+verglichen werden müssen und es ausgegeben wird, wieviele "Richtige" er hat.
+
+
+1. User-Eingaben der 6 getippten Zahlen => Speicherung in einem Array
+
+2. 6 zufällige Zahlen zwischen 1 und 49 "ziehen" => Speicherung in einem neuen Array
+
+3. Vergleich der getippten und gezogenen Zahlen => Speicher der Anzahl der "Richtigen"
+
+4. Ausgabe der gezogenen Zahlen, der getippten Zahlen und der Anzahl der "Richtigen"
+        */
 class Program
 {
   static void Main(string[] args)
   {
-    Console.WriteLine("Hello, Lotto!");
+        
+
+        //Zahlenarray erstellung
+
+        int[] zahlen = new int[49];
+
+
+        //Zahleneingabe
+        Console.WriteLine("Geben sie 6 Zahlen ein:\n");
+
+    
 
   }
 }
